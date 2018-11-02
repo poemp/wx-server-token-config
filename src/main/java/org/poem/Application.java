14205943b06dbb1aa8172512b462f46dd947ab1f
lywworking <xue_2013@sina.com>
2018-11-02 15:03:00 +0800

    s

--- src/main/java/org/poem/Application.java
+++ src/main/java/org/poem/Application.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
